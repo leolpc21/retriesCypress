d66815c8c824a3c7e1a85972cbcef55b70dbf72e
Leo Costa <leolpc21@hotmail.com>
2024-04-27 10:56:21 -0300

    add config cloud cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "wo4tzb",
     retries: 1,
     fixturesFolder: false,
     setupNodeEvents(on, config) {
